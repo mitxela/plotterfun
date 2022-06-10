f654e8cf254920538cf19b6b33e52675d34ea56a
Tim Koop <github@timkoop.com>
2022-06-09 19:15:48 -0500

    Removed setTimeout and fixed some randomness

--- jaggy.js
+++ jaggy.js
@@ -9,16 +9,21 @@ postMessage(['sliders', defaultControls.concat([
 ])]);
 
 
-// Thanks to https://gist.github.com/tionkje/6ab66360dcfe9a9e2b5560742d259389
+// Thanks to https://gist.github.com/blixt/f17b47c62508be59987b
 function createRandFunction(seedString) {
-    for (var i = 0, h = 1779033703 ^ seedString.length; i < seedString.length; i++) {
-        h = Math.imul(h ^ seedString.charCodeAt(i), 3432918353);
-        h = h << 13 | h >>> 19;
-    } return function () {
-        h = Math.imul(h ^ (h >>> 16), 2246822507);
-        h = Math.imul(h ^ (h >>> 13), 3266489909);
-        return ((h ^= h >>> 16) >>> 0) / 4294967295;
-    }
+    let seed  = parseInt(seedString) % 2147483647
+    if (seed <= 0) seed += 2147483646;
+
+    let next = function() {
+        return seed = seed * 16807 % 2147483647;
+    };
+    
+    let rand = function() {
+        // We know that result of next() will be 1 to 2147483646 (inclusive).
+        return (next() - 1) / 2147483646;
+    };
+
+    return rand
 }
 
 // These three functions thanks to https://stackoverflow.com/a/1501725/650004
@@ -58,79 +63,76 @@ function findIndexOfClosestLineToPoint(loop, point) {
 
 onmessage = async function (e) {
 
-    setTimeout(function() {
-        const [config, pixData] = e.data;
-    
-        // Image processor (gets a value for the current pixel)
-        const getPixel = pixelProcessor(config, pixData)
-    
-        // Our points.  Not the final list given back.
-        let points = [];
-    
-        // We will use a random number generater that can take a seed, so that all dots are the same
-        // when changing line directions
-        let dotRand = createRandFunction("" + config.Seed);
-    
-        for (let y = 0; y < config.height - 1; y += 1) {
-            for (let x = 0; x <= config.width - 1; x += 1) {
-                // Get the current pixels 'value'
-                pixelval = getPixel(x, y);
-    
-                // We will place a dot here, randomly, with a darker value more likely to be placed
-                p = pixelval / 255.0    // convert pixel darkness to a probablilty (p)
-                p = p * p * p * 0.1 // p value is still too high, so we turn it down to something that looks about right
-    
-                if (p > dotRand()) {
-                    points.push([x, y]);
-                }
-    
-            }
-        }
-    
-        let totalPoints = points.length
-        let everyPercent = Math.ceil(totalPoints / 100)
-        let everyRedraw = Math.ceil(totalPoints / 10)
-    
-        // This loop is an ordered array of points
-        let loop = [];
-    
-        let randomPointIndex = parseInt(Math.random() * points.length);
-        loop.push(points[randomPointIndex]);
-        points.splice(randomPointIndex, 1);
-    
-        randomPointIndex = parseInt(Math.random() * points.length);
-        loop.push(points[randomPointIndex]);
-        points.splice(randomPointIndex, 1);
-    
-        randomPointIndex = parseInt(Math.random() * points.length);
-        loop.push(points[randomPointIndex]);
-        points.splice(randomPointIndex, 1);
-    
-        loop.push(loop[0]);
-    
-        // loop is now a triangle, with four points.  The first and last are the same point.
-    
-        while(points.length > 0) {
-            randomPointIndex = parseInt(Math.random() * points.length)
-            let point = points[randomPointIndex]
-    //        points.splice(randomPointIndex, 1)
-            // The following two lines act like the above splice, but maybe faster
-            points[randomPointIndex] = points[points.length-1]
-            points.length = points.length - 1
-    
-            let indexOfClosestLineToPoint = findIndexOfClosestLineToPoint(loop, point)
-            loop.splice(indexOfClosestLineToPoint + 1, 0, point)
-    
-            if (points.length % everyPercent == 0) {
-                postMessage(['msg', ((totalPoints - points.length) * 100 / totalPoints).toFixed(0) + "%"]);
+    const [config, pixData] = e.data;
+
+    // Image processor (gets a value for the current pixel)
+    const getPixel = pixelProcessor(config, pixData)
+
+    // Our points.  Not the final list given back.
+    let points = [];
+
+    // We will use a random number generater that can take a seed, so that all dots are the same
+    // when changing line directions
+    let dotRand = createRandFunction("" + config.Seed);
+
+    for (let y = 0; y < config.height - 1; y += 1) {
+        for (let x = 0; x <= config.width - 1; x += 1) {
+            // Get the current pixels 'value'
+            pixelval = getPixel(x, y);
+
+            // We will place a dot here, randomly, with a darker value more likely to be placed
+            p = pixelval / 255.0    // convert pixel darkness to a probablilty (p)
+            p = p * p * p * 0.1 // p value is still too high, so we turn it down to something that looks about right
+
+            if (p > dotRand()) {
+                points.push([x, y]);
             }
 
-            if (points.length % everyRedraw == 0) {
-                postLines(loop);
-            }
         }
-        postMessage(['msg', '']);
-        postLines(loop);
+    }
 
-    }, 1)
+    let totalPoints = points.length
+    let everyPercent = Math.ceil(totalPoints / 100)
+    let everyRedraw = Math.ceil(totalPoints / 10)
+
+    // This loop is an ordered array of points
+    let loop = [];
+
+    let randomPointIndex = parseInt(dotRand() * points.length);
+    loop.push(points[randomPointIndex]);
+    points.splice(randomPointIndex, 1);
+
+    randomPointIndex = parseInt(dotRand() * points.length);
+    loop.push(points[randomPointIndex]);
+    points.splice(randomPointIndex, 1);
+
+    randomPointIndex = parseInt(dotRand() * points.length);
+    loop.push(points[randomPointIndex]);
+    points.splice(randomPointIndex, 1);
+
+    loop.push(loop[0]);
+
+    // loop is now a triangle, with four points.  The first and last are the same point.
+
+    while(points.length > 0) {
+        randomPointIndex = parseInt(dotRand() * points.length)
+        let point = points[randomPointIndex]
+//        points.splice(randomPointIndex, 1)
+        // The following two lines act like the above splice, but maybe faster
+        points[randomPointIndex] = points[points.length-1]
+        points.length = points.length - 1
+
+        let indexOfClosestLineToPoint = findIndexOfClosestLineToPoint(loop, point)
+        loop.splice(indexOfClosestLineToPoint + 1, 0, point)
+
+        if (points.length % everyPercent == 0) {
+            postMessage(['msg', ((totalPoints - points.length) * 100 / totalPoints).toFixed(0) + "%"]);
+        }
+
+        if (points.length % everyRedraw == 0) {
+            postLines(loop);
+        }
+    }
+    postMessage(['msg', '']);
+    postLines(loop);
 }
