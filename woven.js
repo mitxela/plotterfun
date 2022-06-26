8c3eba8859fb54efcc7cd7706f452f85250ab0f1
J-Waal <J.R.deWaal@student.tudelft.nl>
2022-06-26 16:51:02 +0200

    add cosine to woven
    cosine interpolation option for woven when line swaps row

--- woven.js
+++ woven.js
@@ -9,6 +9,7 @@ importScripts('helpers.js')
 postMessage(['sliders', defaultControls.concat([
 	{label: 'Frequency', value: 150, min: 5, max: 256},
 	{label: 'Line Count', value: 8, min: 5, max: 200},
+	{label: 'Cosine', type: 'checkbox' },
 ])]);
 
 
@@ -19,6 +20,7 @@ onmessage = function(e) {
 	const lineCount = config['Line Count'];
 	const frequency = config.Frequency;
 	const incr_y = Math.floor(config.height / lineCount);
+	const cosine = config.Cosine;
 	output = [];
 
 	let order = Array.from(Array(lineCount).keys())
@@ -55,8 +57,20 @@ onmessage = function(e) {
 	lines = []
 	for (let l = 0; l<lineCount; l++){
 		let line = []
-		for (let x = 0; x < output.length; x++) {
-			//
+		line.push([0, l*incr_y])
+		for (let x = 1; x < output.length; x++) {
+			if (cosine && lineMap[x-1][l] != lineMap[x][l]){
+				// nice cosine function to connect the two lines
+				let h1 = lineMap[x-1][l]*incr_y
+				let h2 = lineMap[x][l]*incr_y
+				let d = h1-h2
+				let a = (h1+h2)/2
+				for (p=1; p<frequency; p++){
+					let xp = (x-1)*frequency+p
+					let yp = a+(d/2)*Math.cos(p*Math.PI/frequency)
+					line.push([xp, yp])
+				}
+			}
 			line.push([x*frequency, lineMap[x][l]*incr_y])
 		}
 		lines.push(line)
