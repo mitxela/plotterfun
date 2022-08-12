b81213513f92c1f813698a9cc99d1e13f50c78d8
J-Waal <101430722+J-Waal@users.noreply.github.com>
2022-08-12 13:58:29 +0200

    Peano cleanup (#2)
    Remove old peano code.
    
    * Delete peano.js
    
    * Rename peano2.js to peano.js
    
    * Update main.htm

--- peano.js
+++ peano.js
@@ -4,10 +4,11 @@
 importScripts('helpers.js')
 
 postMessage(['sliders', defaultControls.concat([
-  {label: 'Order', value: 3, min: 1, max: 6}, // does nothing
+  {label: 'Order', value: 3, min: 1, max: 6},
+  {label: 'Hblocks', value: 1, min: 1, max: 15},
+  {label: 'Vblocks', value: 1, min: 1, max: 15},
 ])]);
 
-// only works if the image is 729 x 729 (=3^5)
 // Peano curve
 
 function sumdarknes(top,left,bottom,right) {
@@ -20,74 +21,77 @@ function sumdarknes(top,left,bottom,right) {
   return sum
 }
 
-function addlevel(order, left, top, line, dir){
+function addlevel(order, left, top, line, dir, scale){
   let size = Math.pow(3,order)
   let right = left + size
   let bottom = top + size
-  let sum = sumdarknes(top,left,bottom,right)
+  let sum = sumdarknes(top*scale[1],left*scale[0],bottom*scale[1],right*scale[0])
+  let pscale = scale[0]*scale[1]
+  sum = sum/pscale
   //console.log("order", order,"dir",dir,"sum",sum)
   let tops = [top, top+Math.pow(3,order-1), top+2*Math.pow(3,order-1)]
   let lefts = [left, left+Math.pow(3,order-1), left+2*Math.pow(3,order-1)]
   if (sum/size > 1530 && order > 1){ // need to change this line to find better threshold
     // recursive splitting of the square
     if (dir == 0){
-      addlevel(order-1, lefts[0], tops[0], line, 0) // segment 1
-      addlevel(order-1, lefts[0], tops[1], line, 3) // segment 2
-      addlevel(order-1, lefts[0], tops[2], line, 0) // segment 3
-      addlevel(order-1, lefts[1], tops[2], line, 1) // segment 4
-      addlevel(order-1, lefts[1], tops[1], line, 2) // segment 5
-      addlevel(order-1, lefts[1], tops[0], line, 1) // segment 6
-      addlevel(order-1, lefts[2], tops[0], line, 0) // segment 7
-      addlevel(order-1, lefts[2], tops[1], line, 3) // segment 8
-      addlevel(order-1, lefts[2], tops[2], line, 0) // segment 9
+      addlevel(order-1, lefts[0], tops[0], line, 0, scale) // segment 1
+      addlevel(order-1, lefts[0], tops[1], line, 3, scale) // segment 2
+      addlevel(order-1, lefts[0], tops[2], line, 0, scale) // segment 3
+      addlevel(order-1, lefts[1], tops[2], line, 1, scale) // segment 4
+      addlevel(order-1, lefts[1], tops[1], line, 2, scale) // segment 5
+      addlevel(order-1, lefts[1], tops[0], line, 1, scale) // segment 6
+      addlevel(order-1, lefts[2], tops[0], line, 0, scale) // segment 7
+      addlevel(order-1, lefts[2], tops[1], line, 3, scale) // segment 8
+      addlevel(order-1, lefts[2], tops[2], line, 0, scale) // segment 9
     }
     if (dir == 1){
-      addlevel(order-1, lefts[0], tops[2], line, 1) // segment 1
-      addlevel(order-1, lefts[1], tops[2], line, 0) // segment 2
-      addlevel(order-1, lefts[2], tops[2], line, 1) // segment 3
-      addlevel(order-1, lefts[2], tops[1], line, 2) // segment 4
-      addlevel(order-1, lefts[1], tops[1], line, 3) // segment 5
-      addlevel(order-1, lefts[0], tops[1], line, 2) // segment 6
-      addlevel(order-1, lefts[0], tops[0], line, 1) // segment 7
-      addlevel(order-1, lefts[1], tops[0], line, 0) // segment 8
-      addlevel(order-1, lefts[2], tops[0], line, 1) // segment 9
+      addlevel(order-1, lefts[0], tops[2], line, 1, scale) // segment 1
+      addlevel(order-1, lefts[1], tops[2], line, 0, scale) // segment 2
+      addlevel(order-1, lefts[2], tops[2], line, 1, scale) // segment 3
+      addlevel(order-1, lefts[2], tops[1], line, 2, scale) // segment 4
+      addlevel(order-1, lefts[1], tops[1], line, 3, scale) // segment 5
+      addlevel(order-1, lefts[0], tops[1], line, 2, scale) // segment 6
+      addlevel(order-1, lefts[0], tops[0], line, 1, scale) // segment 7
+      addlevel(order-1, lefts[1], tops[0], line, 0, scale) // segment 8
+      addlevel(order-1, lefts[2], tops[0], line, 1, scale) // segment 9
     }
     if (dir == 2){
-      addlevel(order-1, lefts[2], tops[2], line, 2) // segment 1
-      addlevel(order-1, lefts[2], tops[1], line, 1) // segment 2
-      addlevel(order-1, lefts[2], tops[0], line, 2) // segment 3
-      addlevel(order-1, lefts[1], tops[0], line, 3) // segment 4
-      addlevel(order-1, lefts[1], tops[1], line, 0) // segment 5
-      addlevel(order-1, lefts[1], tops[2], line, 3) // segment 6
-      addlevel(order-1, lefts[0], tops[2], line, 2) // segment 7
-      addlevel(order-1, lefts[0], tops[1], line, 1) // segment 8
-      addlevel(order-1, lefts[0], tops[0], line, 2) // segment 9
+      addlevel(order-1, lefts[2], tops[2], line, 2, scale) // segment 1
+      addlevel(order-1, lefts[2], tops[1], line, 1, scale) // segment 2
+      addlevel(order-1, lefts[2], tops[0], line, 2, scale) // segment 3
+      addlevel(order-1, lefts[1], tops[0], line, 3, scale) // segment 4
+      addlevel(order-1, lefts[1], tops[1], line, 0, scale) // segment 5
+      addlevel(order-1, lefts[1], tops[2], line, 3, scale) // segment 6
+      addlevel(order-1, lefts[0], tops[2], line, 2, scale) // segment 7
+      addlevel(order-1, lefts[0], tops[1], line, 1, scale) // segment 8
+      addlevel(order-1, lefts[0], tops[0], line, 2, scale) // segment 9
     }
     if (dir == 3){
-      addlevel(order-1, lefts[2], tops[0], line, 3) // segment 1
-      addlevel(order-1, lefts[1], tops[0], line, 2) // segment 2
-      addlevel(order-1, lefts[0], tops[0], line, 3) // segment 3
-      addlevel(order-1, lefts[0], tops[1], line, 0) // segment 4
-      addlevel(order-1, lefts[1], tops[1], line, 1) // segment 5
-      addlevel(order-1, lefts[2], tops[1], line, 0) // segment 6
-      addlevel(order-1, lefts[2], tops[2], line, 3) // segment 7
-      addlevel(order-1, lefts[1], tops[2], line, 2) // segment 8
-      addlevel(order-1, lefts[0], tops[2], line, 3) // segment 9
+      addlevel(order-1, lefts[2], tops[0], line, 3, scale) // segment 1
+      addlevel(order-1, lefts[1], tops[0], line, 2, scale) // segment 2
+      addlevel(order-1, lefts[0], tops[0], line, 3, scale) // segment 3
+      addlevel(order-1, lefts[0], tops[1], line, 0, scale) // segment 4
+      addlevel(order-1, lefts[1], tops[1], line, 1, scale) // segment 5
+      addlevel(order-1, lefts[2], tops[1], line, 0, scale) // segment 6
+      addlevel(order-1, lefts[2], tops[2], line, 3, scale) // segment 7
+      addlevel(order-1, lefts[1], tops[2], line, 2, scale) // segment 8
+      addlevel(order-1, lefts[0], tops[2], line, 3, scale) // segment 9
     }
     // different order to visit the new squares
-  }
-  // add last point
-  if (dir == 0){
-    line.push([right,bottom])
-  }
-  if (dir == 1){
-    line.push([right,top])
-  }
-  if (dir == 2){
-    line.push([left,top])
-  }
-  if (dir == 3){
-    line.push([left,bottom])
+  } else {
+    // add last point
+    if (dir == 0){
+      line.push([right*scale[0],bottom*scale[1]])
+    }
+    if (dir == 1){
+      line.push([right*scale[0],top*scale[1]])
+    }
+    if (dir == 2){
+      line.push([left*scale[0],top*scale[1]])
+    }
+    if (dir == 3){
+      line.push([left*scale[0],bottom*scale[1]])
+    }
   }
 }
 
@@ -95,7 +99,36 @@ onmessage = function(e) {
   const [ config, pixData ] = e.data;
   getPixel = pixelProcessor(config, pixData)
 
-  line = [[0,0]] // add first point
-  addlevel(6,0,0,line, 0) // call recursive function
+  const maxorder = config.Order;
+  const hblocks = config.Hblocks;
+  const vblocks = config.Vblocks;
+
+
+  maxsize = Math.pow(3,maxorder)
+  let hscale = config.width/(maxsize*hblocks)
+  let vscale = config.height/(maxsize*vblocks)
+  let scale = [hscale,vscale] // scale represents mismatch between pixels and line units
+
+
+  let heven = ((hblocks+1)%2)
+  veven = ((vblocks+1)%2)
+  line = [[veven*hscale*maxsize,heven*config.height]] // add first point
+  if (heven){
+    for (let vblock = vblocks-1; vblock >= 0; vblock--){
+      addlevel(maxorder,0,vblock*maxsize,line, ((vblock)%2)+1,scale)
+    }
+  }
+  
+  for (let vblock = 0; vblock < vblocks; vblock++){
+    if (!(vblock%2)){
+      for (let hblock = heven; hblock < hblocks; hblock++) {
+        addlevel(maxorder,hblock*maxsize,vblock*maxsize,line, ((hblock+heven)%2),scale)
+      }
+    } else {
+      for (let hblock = hblocks-1; hblock >= heven; hblock--) {
+        addlevel(maxorder,hblock*maxsize,vblock*maxsize,line, ((hblock+heven+1)%2)+2,scale)
+      }
+    }
+  }
   postLines(line);
 }
